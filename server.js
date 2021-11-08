2e185ce4054421fbd896bee2c8755e331e3ed5aa
joycesand <ajoycesandr@gmail.com>
2021-11-08 19:11:07 +0300

    rendered html in node

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const session = require('express-session');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const Register = require('./models/registrationModel')
-const app = express();
-
 
 // Middle ware
-app.set('view engine', 'pug');
+var cons = require('consolidate');
+// view engine setup
+var app = express();
+app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,13 +29,13 @@ app.use(session({
 passport.use(new LocalStrategy(Register.authenticate()));
 passport.serializeUser(function(user, done) {
     done(null, user.id);
-  });
+});
 
 passport.deserializeUser(function(id, done) {
     Register.findById(id, function(err, user) {
-      done(err, user);
+        done(err, user);
     });
-  });
+});
 
 //mongoose db connection
 mongoose.connect("mongodb://localhost:27017/demo-db", { useNewUrlParser: true, useUnifiedTopology: true });
@@ -47,7 +50,7 @@ app.use('/login', loginRoutes);
 //logout
 app.post('/logout', (req, res) => {
     if (req.session) {
-        req.session.destroy(function (err) {
+        req.session.destroy(function(err) {
             if (err) {
                 // failed to destroy session
             } else {
@@ -55,16 +58,10 @@ app.post('/logout', (req, res) => {
             }
         })
     }
-    
+
 })
 
 // Listening for requests: the server!
 app.listen(3001, () => {
     console.log('Server is listening.....');
 });
-
-
-
-
-
-
